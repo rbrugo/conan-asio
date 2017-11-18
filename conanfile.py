23727b6a59e4c19c62a7530df3fbe1b324853b52
Felix Morgner <felix.morgner@gmail.com>
2017-11-18 18:13:48 +0100

    cleanup code style and fix description

--- conanfile.py
+++ conanfile.py
@@ -1,30 +1,51 @@
 from conans import ConanFile, tools, os
 
+
 class AsioConan(ConanFile):
     name = "Asio"
     version = "1.10.8"
     url = "https://github.com/bincrafters/conan-asio"
-    description = "Header only C++ library that implements RFC6455 The WebSocket Protocol"
+    description = (
+        "Asio is a cross-platform C++ library for network and low-level I/O "
+        "programming that provides developers with a consistent asynchronous "
+        "model using a modern C++ approach."
+    )
     license = "https://github.com/chriskohlhoff/asio/blob/master/asio/LICENSE_1_0.txt"
-    options = {"shared": [True, False], "with_boost_regex": [True, False], "with_openssl": [True, False]}
-    default_options = "shared=False", "with_boost_regex=False", "with_openssl=False"
-    
+    options = {
+        "shared": [True, False],
+        "with_boost_regex": [True, False],
+        "with_openssl": [True, False]
+    }
+    default_options = (
+        "shared=False",
+        "with_boost_regex=False",
+        "with_openssl=False"
+    )
+
     def requirements(self):
         if self.options.with_boost_regex:
             self.requires.add("Boost.Regex/1.64.0@bincrafters/stable")
 
         if self.options.with_openssl:
             self.requires.add("OpenSSL/1.0.2@conan/stable")
-                      
+
     def source(self):
         source_url = "https://github.com/chriskohlhoff/asio"
         archive_name = "asio-" + self.version.replace(".", "-")
         tools.get("{0}/archive/{1}.tar.gz".format(source_url,  archive_name))
 
     def package(self):
-        extracted_dir = self.name + "-" + self.name + "-" + self.version.replace(".", "-")
-        include_dir = os.path.join(extracted_dir, self.name, "include")
-        self.copy(pattern="*.hpp", dst="include", src=include_dir)		
+        extracted_dir = "{0}-{0}-{1}".format(
+            self.name.lower(),
+            self.version.replace('.', '-')
+        )
+        include_dir = os.path.join(
+            extracted_dir,
+            self.name.lower(),
+            "include"
+        )
+        print(include_dir)
+        self.copy(pattern="*.hpp", dst="include", src=include_dir)
 
     def package_id(self):
         self.info.header_only()
